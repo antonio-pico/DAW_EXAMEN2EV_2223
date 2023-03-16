2bdbf8b6dd02d09fc7852678a85e512d6573ef8d
dccdaw <davclecol@alu.edu.gva.es>
2023-03-16 20:27:40 +0100

    Pruebas unitarias

---
+++ Loto/LotoTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LotoTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LotoTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("93e23fb8-7603-423e-a9b1-a31045ef37eb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Loto/LotoTest/UnitTest1.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using LotoClassNS;
+
+namespace LotoTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void ValidarLotoMenor()
+        {
+            bool esperado = false;
+
+            int[] misNums = new int[6];
+            misNums[0] = 0;
+            misNums[1] = 2;
+            misNums[2] = 3;
+            misNums[3] = 4;
+            misNums[4] = 5;
+            misNums[5] = 6;
+
+            loto loteria = new loto(misNums);
+
+            Assert.AreEqual(esperado, loteria.CombinacionValida, "Número menor de 1");
+
+        }
+
+        [TestMethod]
+        public void ValidarLotoMayor()
+        {
+            bool esperado = false;
+
+            int[] misNums = new int[6];
+            misNums[0] = 50;
+            misNums[1] = 2;
+            misNums[2] = 3;
+            misNums[3] = 4;
+            misNums[4] = 5;
+            misNums[5] = 6;
+
+            loto loteria = new loto(misNums);
+
+            Assert.AreEqual(esperado, loteria.CombinacionValida, "Número mayor de 49");
+
+        }
+
+        [TestMethod]
+        public void ValidarLotoCorrecto()
+        {
+            bool esperado = true;
+
+            int[] misNums = new int[6];
+            misNums[0] = 1;
+            misNums[1] = 2;
+            misNums[2] = 3;
+            misNums[3] = 4;
+            misNums[4] = 5;
+            misNums[5] = 6;
+
+            loto loteria = new loto(misNums);
+
+            Assert.AreEqual(esperado, loteria.CombinacionValida, "Número incorrecto");
+
+        }
+
+        [TestMethod]
+        public void ValidarLotoNumRepetido()
+        {
+            bool esperado = false;
+
+            int[] misNums = new int[6];
+            misNums[0] = 1;
+            misNums[1] = 1;
+            misNums[2] = 3;
+            misNums[3] = 4;
+            misNums[4] = 5;
+            misNums[5] = 6;
+
+            loto loteria = new loto(misNums);
+
+            Assert.AreEqual(esperado, loteria.CombinacionValida, "Número repetido");
+
+        }
+
+    }
+}
